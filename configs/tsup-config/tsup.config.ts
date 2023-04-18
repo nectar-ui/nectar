8c3dea3b29e2521d0d25dbc0c1b704f71ecbe089
Brennan Walsh <mail@brennanwal.sh>
2023-04-17 22:21:08 -0700

    Fixed Jest bug.

--- configs/tsup-config/tsup.config.ts
+++ configs/tsup-config/tsup.config.ts
@@ -5,8 +5,7 @@ export default defineConfig({
 	entry: ['src/index.ts'],
 	plugins: [cssModulesPlugin({ inject: true })],
 	minify: true,
-	onSuccess: 'tsc',
-	clean: true,
-	format: 'esm,cjs',
+	onSuccess: 'tsc -b',
+	format: ['cjs', 'esm'],
 	target: 'es2020'
 })
