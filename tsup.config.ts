e9005d79880a6113376d3f3933bd13e8c6b3ab68
Brennan Walsh <mail@brennanwal.sh>
2023-04-18 19:17:05 -0700

    Switched to testing via ava.

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,10 +1,13 @@
 import cssModulesPlugin from 'esbuild-css-modules-plugin'
+import path from 'path'
 import { defineConfig } from 'tsup'
 
 export default defineConfig({
 	entry: ['src/index.ts'],
 	plugins: [cssModulesPlugin({ inject: true })],
 	minify: true,
+	tsconfig: path.resolve(__dirname, './tsconfig.base.json'),
+
 	onSuccess: 'tsc -b',
 	format: ['cjs', 'esm'],
 	target: 'es2020',
