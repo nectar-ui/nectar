c68c9b3fbbef7347597ea5f6ca7bd795c7ccefb5
Brennan Walsh <mail@brennanwal.sh>
2023-05-25 23:32:18 -0700

    .

--- tsup.config.ts
+++ tsup.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from 'tsup'
 
 export default defineConfig({
 	entry: ['src/index.ts'],
-	plugins: [CssModulesPlugin({ inject: true, generateTsFile: true })],
+	plugins: [CssModulesPlugin({ generateTsFile: true })],
 	minify: true,
 	tsconfig: path.resolve(__dirname, './tsconfig.base.json'),
 	onSuccess: 'tsc -b',
